9c3eef8024bac2b69720ed6410206916123633d8
FabioSntos <fabio_santos_2001@outlook.com>
2022-06-30 22:21:49 -0300

    .

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,8 +1,8 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +13,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/anime")
+@RequestMapping("/animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
     private final DateUtil dateUtil;
-    @GetMapping("/list")
+    private final AnimeService animeService;
+    @GetMapping
     public List<Anime> list(){
         log.info(dateUtil.formatLocalTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Naruto"), new Anime("Bleach"));
+        return animeService.listAll();
     };
 }
--- src/main/java/academy/devdojo/springboot2/util/DateUtil.java
+++ src/main/java/academy/devdojo/springboot2/util/DateUtil.java
@@ -1,8 +1,5 @@
 package academy.devdojo.springboot2.util;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
