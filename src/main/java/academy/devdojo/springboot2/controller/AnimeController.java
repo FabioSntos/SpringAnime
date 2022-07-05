5cd7c13d5a79b6c4f503382b30a4636cd5640ff7
FabioSntos <fabio_santos_2001@outlook.com>
2022-07-05 19:31:09 -0300

    find animes by id

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -5,7 +5,10 @@ import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +23,14 @@ public class AnimeController {
     private final DateUtil dateUtil;
     private final AnimeService animeService;
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return ResponseEntity.ok( animeService.listAll());
+    };
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        log.info(dateUtil.formatLocalTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok( animeService.findById(id));
     };
 }
