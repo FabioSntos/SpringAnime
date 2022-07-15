62f1e7b9e4b30dc5070e9e4f7528f39ae6265c9f
FabioSntos <fabio_santos_2001@outlook.com>
2022-07-14 21:08:34 -0300

    docker configuration

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -5,13 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService{
 
-    private final List<Anime> animes = List.of(new Anime(1L,"Naruto"), new Anime(2L,"Bleach"));
-
+    private static final List<Anime> animes;
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L,"Naruto"), new Anime(2L,"Bleach")));
+    }
     public List<Anime> listAll(){
 
         return animes;
@@ -22,5 +26,20 @@ public class AnimeService{
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime){
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 100));
+        animes.add(anime);
+        return anime;
+    }
+
+    public void delete(long id){
+        animes.remove(findById(id));
+    }
+
+    public void replace(Anime anime) {
+        delete(anime.getId());
+        animes.add(anime);
     }
 }
